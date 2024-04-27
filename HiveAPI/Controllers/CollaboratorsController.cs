74977512e03e90543c8cd6fc5c17ff97aba6a2b0
Redi <75811751+Ctrl-Yll@users.noreply.github.com>
2024-04-27 20:20:29 +0200

    Made some changes to collaborator controller adding authorize, changing url for get by email and by workspaceId also include workspace on get by email

--- HiveAPI/Controllers/CollaboratorsController.cs
+++ HiveAPI/Controllers/CollaboratorsController.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using HiveAPI.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HiveAPI.Controllers
 {
@@ -18,6 +20,7 @@ namespace HiveAPI.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetCollaborators()
         {
             var collaborators = await _context.Collaborators.ToListAsync();
@@ -25,6 +28,7 @@ namespace HiveAPI.Controllers
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<IActionResult> GetCollaboratorById(int id)
         {
             var collaborator = await _context.Collaborators.FindAsync(id);
@@ -34,11 +38,28 @@ namespace HiveAPI.Controllers
             return Ok(collaborator);
         }
 
-        [HttpGet("by-email")]
-        public async Task<IActionResult> GetCollaboratorsByEmail(string email)
+        [HttpGet("by-email/{userEmail}")]
+        [Authorize]
+        public async Task<IActionResult> GetCollaboratorsByEmail(string userEmail)
+        {
+
+            var collaborators = await _context.Collaborators
+                .Include(c => c.WorkSpace)
+                .Where(c => c.UserEmail == userEmail)
+                .ToListAsync();
+
+            if (collaborators.Count == 0)
+                return NotFound();
+
+            return Ok(collaborators);
+        }
+
+        [HttpGet("by-workspaceId/{workspaceId}")]
+        [Authorize]
+        public async Task<IActionResult> GetCollaboratorsByWorkspaceId(int workspaceId)
         {
             var collaborators = await _context.Collaborators
-                .Where(c => c.UserEmail == email)
+                .Where(c => c.WorkSpaceId == workspaceId)
                 .ToListAsync();
 
             if (collaborators.Count == 0)
@@ -48,14 +69,31 @@ namespace HiveAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateCollaborator(Collaborator collaborator)
+        [Authorize]
+        public async Task<IActionResult> CreateCollaborator([FromBody] Collaborator collaborator)
         {
+            var workspace = await _context.WorkSpaces.FirstOrDefaultAsync(ws => ws.WId == collaborator.WorkSpaceId);
+
+            if (workspace == null)
+            {
+                return BadRequest("Workspace not found");
+            }
+
+            var existingCollaborator = await _context.Collaborators.FirstOrDefaultAsync(c => c.UserEmail == collaborator.UserEmail && c.WorkSpaceId == collaborator.WorkSpaceId);
+            if (existingCollaborator != null)
+            {
+                return Conflict("The Collaborator already exists");
+            }
+
+            collaborator.WorkSpace = workspace;
+
             _context.Collaborators.Add(collaborator);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetCollaboratorById), new { id = collaborator.CollaboratorId }, collaborator);
         }
 
         [HttpPut("{id}")]
+        [Authorize]
         public async Task<IActionResult> UpdateCollaborator(int id, Collaborator updatedCollaborator)
         {
             var existingCollaborator = await _context.Collaborators.FindAsync(id);
@@ -71,6 +109,7 @@ namespace HiveAPI.Controllers
 
     
         [HttpDelete("{id}")]
+        [Authorize]
         public async Task<IActionResult> DeleteCollaborator(int id)
         {
             var collaborator = await _context.Collaborators.FindAsync(id);
