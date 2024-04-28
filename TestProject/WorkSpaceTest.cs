a6e9a033ff30192c4ca464665e7695ba04cd5938
AltinaKasabaqi <altinakasabaqi@gmail.com>
2024-04-28 14:12:21 +0200

    WrokSpaceGetMethodTesting - not working :(

---
+++ TestProject/WorkSpaceTest.cs
@@ -0,0 +1,44 @@
+﻿using HiveAPI.Controllers;
+using HiveAPI;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class WorkSpaceTests
+    {
+
+        private readonly Mock<WorkSpacesController> _workSpaceController;
+        private readonly Mock<APIDbContext> _context;
+
+        public WorkSpaceTests()
+        {
+            _workSpaceController = new Mock<WorkSpacesController>();
+            _context = new Mock<APIDbContext>();
+
+        }
+        [Fact]
+        public void GetUserReturnsUserWhenUserExists()
+        {
+            // Arrange
+            var mockController = new Mock<WorkSpacesController>();
+            //var existingUser = new User { UserId = 1, name = "John", email = "john@example.com" };
+            //mockController.Setup(c => c.Create(existingUser));
+
+            var result = mockController.Setup(c => c.GetWorkSpaces());
+
+            //var controller = new UserController(null, mockController.Object);
+
+            // Act
+
+
+            // Assert
+            Assert.NotEmpty(result);
+        }
+
+    }
+}
